ff17215348b5a049d143c0e69d797af0a8c591e8
listanegra <5874907+listanegra@users.noreply.github.com>
2021-10-13 00:31:41 -0300

    Determine MIME type from file extension

--- src/io/UnpackedArchiveRepository.cpp
+++ src/io/UnpackedArchiveRepository.cpp
@@ -1,9 +1,47 @@
 #if defined(DEBUG_ENVIRONMENT)
 #include "ArchiveRepository.hpp"
 
+#include "../Utils.hpp"
+
 #include <cstdio>
 #include <sstream>
 #include <stdexcept>
+
+static std::string GetMimeType(const std::string &fileName) {
+#define MIME_TYPE(extension, mimeType)        \
+    if (Utils::EndsWith(fileName, extension)) \
+        return mimeType;
+
+    /* Audio Files */
+    MIME_TYPE(".wav", "audio/wav");
+    MIME_TYPE(".ogg", "audio/ogg");
+    MIME_TYPE(".mp3", "audio/mpeg");
+    MIME_TYPE(".opus", "audio/opus");
+
+    /* Text Files */
+    MIME_TYPE(".css", "text/css");
+    MIME_TYPE(".htm", "text/html");
+    MIME_TYPE(".html", "text/html");
+
+    /* Image Files */
+    MIME_TYPE(".png", "image/png");
+    MIME_TYPE(".jpg", "image/jpeg");
+    MIME_TYPE(".jpeg", "image/jpeg");
+    MIME_TYPE(".svg", "image/svg+xml");
+
+    /* Font Files */
+    MIME_TYPE(".otf", "font/otf");
+    MIME_TYPE(".ttf", "font/ttf");
+    MIME_TYPE(".woff", "font/woff");
+    MIME_TYPE(".woff2", "font/woff2");
+
+    /* Application Files */
+    MIME_TYPE(".json", "application/json");
+    MIME_TYPE(".js", "application/javascript");
+
+    /* Default */
+    return "text/plain";
+}
 
 ArchiveRepository::ArchiveRepository(const std::string &package)
     : package(ASSETS_FOLDER + package) {}
@@ -39,7 +77,7 @@ Asset ArchiveRepository::Read(const std::string &fileName) {
     Asset asset;
 
     asset.data = buffer;
-    asset.mimeType = "";
+    asset.mimeType = GetMimeType(fileName);
 
     return asset;
 }
